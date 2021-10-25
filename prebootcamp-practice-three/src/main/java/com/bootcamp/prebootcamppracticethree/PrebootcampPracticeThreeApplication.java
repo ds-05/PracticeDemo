11fc4903ba5c6383858aeade9beaf80a07ce727e
Divya Singh <Divya.Singh5@gds.ey.com>
2021-10-25 21:23:48 +0530

    my practice demo

---
+++ prebootcamp-practice-three/src/main/java/com/bootcamp/prebootcamppracticethree/PrebootcampPracticeThreeApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.prebootcamppracticethree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrebootcampPracticeThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrebootcampPracticeThreeApplication.class, args);
+	}
+
+}
---
+++ prebootcamp-practice-three/src/test/java/com/bootcamp/prebootcamppracticethree/PrebootcampPracticeThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.prebootcamppracticethree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrebootcampPracticeThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
